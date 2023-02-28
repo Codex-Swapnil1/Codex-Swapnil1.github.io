2db632c179bcf6e24f1f4b31c6fe3455f9bdc983
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2023-02-28 21:49:50 +0530

    profile issue solved

--- src/Components/Home/BannerDesktop.jsx
+++ src/Components/Home/BannerDesktop.jsx
@@ -12,7 +12,7 @@ export const BannerDesktop = () => {
         <Code/>
         </div>
       <div style={{ width:"40%"}}>
-   <img style={{ maxWidth:"250px",borderRadius:"10px",boxShadow: "rgba(212,212,212, 0.3) 0px 0px 3px 3px"}}  src="./swapnilformal-prev1-removebg-preview.jpg" alt="profile" width="100%" />
+   <img style={{ maxWidth:"250px",borderRadius:"10px",boxShadow: "rgba(212,212,212, 0.3) 0px 0px 3px 3px"}}  src="./swapnilformal.jpg" alt="profile" width="100%" />
       </div>
       </div>
     )
--- src/Components/Home/BannerMobile.jsx
+++ src/Components/Home/BannerMobile.jsx
@@ -8,7 +8,7 @@ export const BannerMobile = () => {
         <Code/>
     </div>
       <div style={{ maxWidth:"350px", paddingTop:"10px",margin:"auto"}}>
-   <img style={{ maxWidth:"300px",borderRadius:"10px",boxShadow: "rgba(212,212,212, 0.3) 0px 0px 3px 3px",marginTop:"30px"}} src="./swapnilformal-prev1-removebg-preview.jpg" alt="programmer" width="100%" />
+   <img style={{ maxWidth:"300px",borderRadius:"10px",boxShadow: "rgba(212,212,212, 0.3) 0px 0px 3px 3px",marginTop:"30px"}} src="./swapnilformal.jpg" alt="programmer" width="100%" />
       </div>
       </div>
     )
